e703fb5989dfa9ce9547424c956dc144b78f6d2c
Tyler <tscizzle@gmail.com>
2018-12-17 14:48:55 -0500

    filled in the variabls, and checked the containment

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 # Step 1: check if target word is in text body
 
 # target word
-bad_ingredient = ''
-## TODO: assign bad_ingredient to the ingredient you're looking for
+bad_ingredient = 'Cocos Nucifera (Coconut) Oil'
 
 # text body
-ingredients_string = ''
-## TODO: assign ingredients_string to the comma-separated list of ingredients
+ingredients_string = 'Water / Eau (Aqua), Cetyl Alcohol, Cetrimonium Chloride, Behentrimonium Chloride, Isopropyl Palmitate, Glycerin, Cyclopentasiloxane, Fragrance / Parfum, Keratin Amino Acids, Cocoyl Hydrolyzed Keratin, Keratin, Hydrolyzed Keratin, Hydrolyzed Collagen, Butyrospermum Parkii (Shea) Butter, Olea Europaea (Olive) Fruit Oil, Cocos Nucifera (Coconut) Oil, Sesamum Indicum (Sesame) Seed Oil, Simmondsia Chinensis (Jojoba) Seed Oil, Hydrolyzed Corn Protein, Hydrolyzed Soy Protein, Dimethicone, Panthenol (Vitamin B5), Phenoxyethanol, Citric Acid, Polyquaternium-55, Stearyl Alcohol'
 
 # result
-ingredient_found = None
-
-## TODO: assign the value of ingredient_found based on bad_ingredient and ingredients_string
-## - assign ingredient_found to False if ingredients_string does not contain bad_ingredient
-## - assign ingredient_found to True if ingredients_string contains bad_ingredient
+ingredient_found = bad_ingredient in ingredients_string
 
 print('Ingredient Found:', ingredient_found)
