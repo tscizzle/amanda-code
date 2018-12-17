d586b677d9788607f5a274f4ce5d88abff813375
Tyler <tscizzle@gmail.com>
2018-12-17 14:28:50 -0500

    planned some steps, and made step 1 question

---
+++ main.py
@@ -0,0 +1,18 @@
+# Step 1: check if target word is in text body
+
+# target word
+bad_ingredient = ''
+## TODO: assign bad_ingredient to the ingredient you're looking for
+
+# text body
+ingredients_string = ''
+## TODO: assign ingredients_string to the comma-separated list of ingredients
+
+# result
+ingredient_found = None
+
+## TODO: assign the value of ingredient_found based on bad_ingredient and ingredients_string
+## - assign ingredient_found to False if ingredients_string does not contain bad_ingredient
+## - assign ingredient_found to True if ingredients_string contains bad_ingredient
+
+print('Ingredient Found:', ingredient_found)
