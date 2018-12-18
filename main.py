d4c4a706cb88e18104b408beb2df6ae4cecd4978
Tyler <tscizzle@gmail.com>
2018-12-18 15:05:48 -0500

    made the answer for step 2

--- main.py
+++ main.py
@@ -1,38 +1,20 @@
 # Step 2: check if multiple target words are in text body
-# Topics info
-# - arrays: https://www.w3schools.com/python/python_arrays.asp
-# - for loops: https://www.w3schools.com/python/python_arrays.asp (scroll down to "Looping Array Elements")
-
-## EXAMPLE: check if any ages in an array are senior citizens
-
-SENIOR_CITIZEN_AGE = 65
-
-citizen_ages = [13, 5, 28, 29, 72, 8]
-
-found_a_senior_citizen = False
-
-for age in citizen_ages:
-
-    if age >= SENIOR_CITIZEN_AGE:
-        found_a_senior_citizen = True
-
-## PROBLEM
 
 # target words
-bad_ingredients = []
-## TODO: replace this line
-## assign bad_ingredients to an array of the ingredients you're looking for
+bad_ingredients = [
+    'Capric/Caprylic Triglycerides (Coconut Oil)',
+    'Cocos Nucifera (Coconut) Fruit Extract',
+    'Cocos Nucifera (Coconut) Water',
+    'Cocos Nucifera (Coconut) Oil'
+]
 
 # text body
-ingredients_string = ''
-## TODO: replace this line
-## assign ingredients_string to the comma-separated list of ingredients
+ingredients_string = 'Water / Eau (Aqua), Cetyl Alcohol, Cetrimonium Chloride, Behentrimonium Chloride, Isopropyl Palmitate, Glycerin, Cyclopentasiloxane, Fragrance / Parfum, Keratin Amino Acids, Cocoyl Hydrolyzed Keratin, Keratin, Hydrolyzed Keratin, Hydrolyzed Collagen, Butyrospermum Parkii (Shea) Butter, Olea Europaea (Olive) Fruit Oil, Cocos Nucifera (Coconut) Oil, Sesamum Indicum (Sesame) Seed Oil, Simmondsia Chinensis (Jojoba) Seed Oil, Hydrolyzed Corn Protein, Hydrolyzed Soy Protein, Dimethicone, Panthenol (Vitamin B5), Phenoxyethanol, Citric Acid, Polyquaternium-55, Stearyl Alcohol'
 
 # result
-ingredient_found = None
-## TODO: replace this line
-## assign the value of ingredient_found based on bad_ingredients and ingredients_string
-## - loop through the array bad_ingredients
-##   - for each ingredient, if the ingredient is in the text body then set ingredient_found to False
+ingredient_found = False
+for ingredient in bad_ingredients:
+    if ingredient in ingredients_string:
+        ingredient_found = True
 
 print('Ingredient Found:', ingredient_found)
