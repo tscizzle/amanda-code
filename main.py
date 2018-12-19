45f722f82cee8a09908f72e40d17695589463bac
Tyler <tscizzle@gmail.com>
2018-12-19 14:52:55 -0500

    step 4 is intro to server, client, and the python package flask to help write a server

---
+++ main.py
@@ -0,0 +1,25 @@
+# Step 4: make a web app with a single web page
+# Topics info
+# - imports: `import package_name` to be able to use other code (written by you, or by somebody else)
+#   - to use someone else's code, you have to download it to your computer before you can use it (we will use pip for downloading)
+#   - (discuss in person)
+# - server / client: code runs in different places. the client is usually the user's computer, the server is somewhere else.
+#   - (discuss in person)
+# - flask: a python package that helps you make a server (http://flask.pocoo.org/)
+#   - you import flask (cuz it's code someone else wrote), then use its functions to make a server.
+
+## EXAMPLE: import the built-in package called math and use one of its functions it gives you
+
+import math
+import your_file
+
+sqrt2 = math.sqrt(2)
+how_many_of_us = your_file.soon_to_be(2)
+
+## PROBLEM
+
+## TODO: follow the example http://flask.pocoo.org/
+# - create a new file, hello.py
+# - install flask and run your server
+# - visit your web page in Chrome
+# - make your web page say "Let me guess. You have a great personality." instead of "Hello World!"
---
+++ your_file.py
@@ -0,0 +1,5 @@
+def soon_to_be(of_us):
+    if of_us == 2:
+        return 3
+    else:
+        return of_us
